d1d5b767ed946790a24d16727ccd43d026ce8b81
Nicholas Ong <nicholas.zhengkai.ong@gmail.com>
2023-05-30 20:17:23 +0800

    Recursive implementation of Strassen algorithm in solidity

--- matrix.sol
+++ matrix.sol
@@ -24,6 +24,17 @@ contract Matrix {
         return c;
     }
 
+    function mult (uint[][] memory a , uint b) public pure returns (uint[][] memory) {
+        uint[][] memory c = new uint[][](a.length);
+        for (uint i=0; i < a.length; i++) {
+            c[i] = new uint[](a[i].length);
+            for (uint j=0; j < a[0].length; j++) {
+                c[i][j] = a[i][j] * b;
+            }
+        }
+        return c;
+    }
+
     function transpose (uint[][] memory a) public pure returns (uint[][] memory) {
         uint[][] memory b = new uint[][](a[0].length);
         for (uint i=0; i<a[0].length; i++) {
@@ -49,5 +60,58 @@ contract Matrix {
             }
         }
         return c;
+    }
+
+    function Strassen(uint256[][] memory a, uint256[][] memory b)
+        public
+        pure
+        returns (uint256[][] memory)
+    {
+        uint256 n = a.length;
+        
+        require(n == b.length, "Matrix sizes do not match");
+        require(n % 2 == 0, "Matrix size must be a power of 2");
+        
+        uint256[][] memory result = new uint256[][](n);
+        for (uint256 i = 0; i < n; i++) {
+            result[i] = new uint256[](n);
+        }
+        
+        multiplyMatrixRecursive(a, b, result, 0, 0, 0, 0, n);
+        
+        return result;
+    }
+    
+    function multiplyMatrixRecursive(
+        uint256[][] memory a,
+        uint256[][] memory b,
+        uint256[][] memory result,
+        uint256 aRow,
+        uint256 aCol,
+        uint256 bRow,
+        uint256 bCol,
+        uint256 size
+    ) private pure {
+        if (size == 1) {
+            result[aRow][bCol] += a[aRow][aCol] * b[bRow][bCol];
+        } else {
+            uint256 newSize = size / 2;
+            
+            // Split matrices into quadrants
+            uint256 aRowMid = aRow + newSize;
+            uint256 aColMid = aCol + newSize;
+            uint256 bRowMid = bRow + newSize;
+            uint256 bColMid = bCol + newSize;
+            
+            // Recursive calls
+            multiplyMatrixRecursive(a, b, result, aRow, aCol, bRow, bCol, newSize);
+            multiplyMatrixRecursive(a, b, result, aRow, aColMid, bRowMid, bCol, newSize);
+            multiplyMatrixRecursive(a, b, result, aRow, aCol, bRow, bColMid, newSize);
+            multiplyMatrixRecursive(a, b, result, aRow, aColMid, bRowMid, bColMid, newSize);
+            multiplyMatrixRecursive(a, b, result, aRowMid, aCol, bRow, bCol, newSize);
+            multiplyMatrixRecursive(a, b, result, aRowMid, aColMid, bRowMid, bCol, newSize);
+            multiplyMatrixRecursive(a, b, result, aRowMid, aCol, bRow, bColMid, newSize);
+            multiplyMatrixRecursive(a, b, result, aRowMid, aColMid, bRowMid, bColMid, newSize);
+        }
     }
 }
